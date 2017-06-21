f5effff9b0213469a9aa72c633e01bf83d288ced
Babatunde Quadre <quadremustapha@gmail.com>
2017-06-21 10:32:53 +0100

    Hello

---
+++ Models/Make.cs
@@ -0,0 +1,14 @@
+namespace vega.Models
+{
+    public class Make
+    {
+        public int Id { get; set; }     
+        public string Name { get; set; }
+        //public ICollection<Model> Models {get; set;}
+
+        public Make()
+        {
+          // Models = new Collection(); 
+        }
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,11 @@
+namespace vega.Models
+{
+    public class Model
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public Make Make { get; set; }
+
+        public int MakeId { get; set; }  
+    }
+}
